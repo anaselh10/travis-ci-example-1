c51cc67214336e822955860879cf4a6cdc6b1d3d
Anas El Hani <elhanianas@gmail.com>
2020-12-07 00:32:48 +0100

    First commit

---
+++ src/test/java/com/example/travisciexample1/TravisCiExample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travisciexample1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravisCiExample1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
